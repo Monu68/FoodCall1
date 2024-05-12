c558368b72b1ef8498ddb0b6ba878c852a2820a7
Rishi-Mohan <jharishimohan@gmail.com>
2024-05-12 17:18:54 +0530

    Api changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,91 +1,108 @@
 import RestaurantCard from "./RestaurantCard.js";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer.js";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus.js";
 
-const Body = () =>{
-    // locat state variable - super powerful variable
-    const [listOfRestaurants, setlistOfRestaurants] = useState([]);
-    const [filteredRestaurant, setfilteredRestaurant] = useState([]);
-    const[searchText , setsearchText] = useState("");
-        
-    useEffect(() => {
-        fetchData();
-    } , []);
+const Body = () => {
+  // locat state variable - super powerful variable
+  const [listOfRestaurent, setlistOfRestaurent] = useState([]); // setting empty as default
+  const [filteredRestaurent, setFilteredRestaurent] = useState([]);
+  const [searchText, setsearchText] = useState("");
 
-    const fetchData = async () =>{
-        const data = await fetch(
-            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-            );
-            
-            const json = await data.json();
-        // optional chaining
-        setlistOfRestaurants(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-        setfilteredRestaurant(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-    };
-    console.log(listOfRestaurants)
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const onlineStatus = useOnlineStatus();
-
-    if(onlineStatus === false)
-        return (
-            <h1>
-                Looks like you're offline !! please check your internet connections
-            </h1>
-            );
-
-
-    return listOfRestaurants.length === 0 ? <Shimmer/> : (      
-        <div className="body">
-            <div className="filter flex">
-                <div className="search m-4 p-4">
-                    <input type="text" 
-                    className="border border-solid border-black" value={searchText}
-                    onChange={(e) =>{
-                        setsearchText(e.target.value);
-
-                    }}/>
-                    <button className="px-4 py-2 bg-gray-100 m-4 rounded-lg hover:bg-sky-200"
-                    onClick={() =>{
-                        //Filter the restaurant cards and update the UI
-                        // searchText
-                        console.log(searchText);
-                        const filteredRestaurant = listOfRestaurants.filter(
-                            (res) => res?.info?.name
-                            ?.toLowerCase().includes(searchText.toLowerCase())
-                        );
-
-                        setfilteredRestaurant(filteredRestaurant);
-                    }} >
-                        Search</button>
-                </div>
-                <div className="search m-4 p-4 items-center ">
-                <button 
-                className="px-4 py-2 bg-gray-100 m-4 rounded-lg hover:bg-sky-200"
-                onClick={() => {
-                    //Filter logic here
-                    const filteredList = listOfRestaurants.filter(
-                        (res) => res.data.avgRating > 4
-                    );
-                    setlistOfRestaurants(filteredList);
-                }
-                }>
-                    Top Rated Restaurants</button>
-                </div>
-            </div>
-            <div className="flex flex-wrap">
-                {filteredRestaurant?.map((restaurant) =>(
-                   <Link key={restaurant?.info.id} 
-                    to={"/restaurants/"+restaurant?.info.id}>
-                        <RestaurantCard  resData={restaurant?.info}/>
-                    </Link> 
-                ))}
-    
-            </div>
-
-        </div>
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.89960&lng=80.22090&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTINGR"
     );
+    const json = await data.json();
+    console.log(json);
+
+    setlistOfRestaurent(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurent(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  if (listOfRestaurent !== undefined && listOfRestaurent?.length === 0) {
+    return <Shimmer />; // calling shimer components
+  }
+
+  //console.log(listOfRestaurants);
+
+  //   const onlineStatus = useOnlineStatus();
+
+  //   if (onlineStatus === false)
+  //     return (
+  //       <h1>
+  //         Looks like you're offline !! please check your internet connections
+  //       </h1>
+  //     );
+
+  return listOfRestaurent.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter flex">
+        <div className="search m-4 p-4">
+          <input
+            type="text"
+            className="border border-solid border-black"
+            value={searchText}
+            onChange={(e) => {
+              setsearchText(e.target.value);
+            }}
+          />
+          <button
+            className="px-4 py-2 bg-gray-100 m-4 rounded-lg hover:bg-sky-200"
+            onClick={() => {
+              //Filter the restaurant cards and update the UI
+              // searchText
+              console.log(searchText);
+              const filteredRestaurent = listOfRestaurent.filter((res) =>
+                res?.info?.name
+                  ?.toLowerCase()
+                  .includes(searchText.toLowerCase())
+              );
+
+              setFilteredRestaurent(filteredRestaurent);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <div className="search m-4 p-4 items-center ">
+          <button
+            className="px-4 py-2 bg-gray-100 m-4 rounded-lg hover:bg-sky-200"
+            onClick={() => {
+              //Filter logic here
+              const filteredList =  listOfRestaurent.filter(
+                (res) => res.data.avgRating > 4
+              );
+              setlistOfRestaurent(filteredList);
+            }}
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-wrap">
+        {filteredRestaurent?.map((restaurant) => (
+          <Link
+            key={restaurant?.info.id}
+            to={"/restaurants/" + restaurant?.info.id}
+          >
+            <RestaurantCard resData={restaurant?.info} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Body;
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,41 +1,50 @@
-
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import { Menu_API } from "../utils/constants";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
 import RestaurantCategory from "./RestaurantCategory";
+import { useState } from "react";
 
-const RestaurantMenu= () =>{
+const RestaurantMenu = () => {
+  const { resId } = useParams(); // resId is comming from path provided "/restaurant/:resId" (ex: "/restaurant/452" or "/restaurant/674"  diffrent for diffrent restaurant to load dynamic page)
 
-    const {resId} = useParams();
+  const resInfo = useRestaurantMenu(resId); // Custom Hook returning API Data matching resId
 
-    const resInfo = useRestaurantMenu(resId);
-    if(resInfo === null) return <Shimmer />;
+  const [showIndex, setShowIndex] = useState(0);
 
-    const {name, cuisines , costForTwoMessage} = resInfo?.cards[0]?.card?.card?.info;
+  // *** till data not fetched show this ***
+  if (resInfo === null) return <Shimmer />;
 
-    const {itemCards} = 
-        resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+  // Destructuring resInfo
+  const { name, cuisines, cloudinaryImageId, costForTwoMessage, avgRating } =
+    resInfo.cards[2].card.card.info;
 
-        const categories = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
-            c => c.card?.card?.["@type"]===
-            "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
-            )
-
-    return(
-        <div className="text-center"> 
-            <h1 className="font-bold my-6 text-2xl">{name}</h1>
-            <p className="font-bold text-lg">
-                {cuisines.join(" ,")} - {costForTwoMessage}
-            </p>
-            {categories.map((category) => (
-                <RestaurantCategory data = {category?.card?.card}/>
-            )
-            )}
-            
-        </div>
+  // *** select only ItemCategory_card data from all cards data ***
+  const categories =
+    resInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards.filter(
+      (c) =>
+        c.card.card?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     );
-};
 
+  return (
+    <div className="text-center">
+      <h1 className="font-bold my-4 text-2xl">{name}</h1>
+      <p className="font-bold text-lg">
+        {cuisines.join(", ")} - {costForTwoMessage}
+      </p>
+
+      {/* categories accordian (dropdown menu) */}
+
+      {categories.map((category, index) => (
+        <RestaurantCategory
+          key={category.card.card.title}
+          data={category.card.card}
+          showItems={true}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default RestaurantMenu;
